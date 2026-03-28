7440c2dda898464e04deb09016822246ff15f5cf
Likitha Puli <likithapuli11@gmail.com>
2026-03-28 15:55:08 +0530

    Fix: moved Google credentials to environment variables

--- config/drive.js
+++ config/drive.js
@@ -1,13 +1,9 @@
 import fs from "fs";
 import { google } from "googleapis";
 
-const credentials = JSON.parse(
-  fs.readFileSync("config/credentials.json")
-);
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-const token = JSON.parse(
-  fs.readFileSync("config/token.json")
-);
+const token = JSON.parse(process.env.GOOGLE_TOKEN);
 
 const { client_secret, client_id, redirect_uris } =
   credentials.installed || credentials.web;
